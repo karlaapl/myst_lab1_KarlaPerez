963a73fa2987245c5e37e87b27af65ac33539d81
Karla Perez <kp190496@gmail.com>
2018-08-20 08:36:08 -0500

    Version correcta hasta  ahorita
    ya sirve Datos

--- Admin_Pasiva.R
+++ Admin_Pasiva.R
@@ -2,9 +2,9 @@
 #ab1
 
 #limpiar el enviroment
-rm(list = lst())
+rm(list = ls())
 
-# Los  0s aceptados anetes de expresar una cifra en notacion cientï¿½fica
+# Los  0s aceptados anetes de expresar una cifra en notacion científica
 options("scipen" = 1000,"digits" = 4)
 
 # Librerias a utilizar
@@ -13,7 +13,7 @@ suppressMessages(library(plotly))
 suppressMessages(library(Quandl)) #descarga de precios
 
 suppressMessages(library(PortfolioAnalytics)) #Teoria modenar de portafolio 
-suppressMessages(library(ROI)) #optimizaciï¿½n para ele portafolio 
+suppressMessages(library(ROI)) #optimización para ele portafolio 
 suppressMessages(library(knitr))
 suppressMessages(library(kableExtra)) #tablas en html 
 options(knitr.table.fromat ="html")
@@ -23,10 +23,12 @@ Quandl.api_key("xNriyB3ufAxdRmZgpq_p") # cargamos el appi key
 Bajarprecios  <- function(Columns, Tickers,Fecha_In,Fecha_Fin) {
   
   # peticion de descarga de datos
-  Datos<- Quandl.datatable(code="WIKI/PRICES",qopts.columns = Columns, ticker = Tickers,date.gte = Fecha_In,date.lte  = Fecha_Fin)
-}
+  
+  Datos <- Quandl.datatable(code="WIKI/PRICES",qopts.columns = Columns, ticker = Tickers,date.gte = Fecha_In, date.lte  = Fecha_Fin)
+
 
 return(Datos) 
+}
 
 tk <-c("TSLA","BBY","HD")
 cs<- c("date", "adj_close")
